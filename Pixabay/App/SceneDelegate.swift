83c944cbe48cfdead5a4a06ee8145eebe4713382
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 19:52:36 +0400

    Added Gallery Assembly as dependency injection

--- Pixabay/App/SceneDelegate.swift
+++ Pixabay/App/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         let window = UIWindow(windowScene: windowScene)
 
-        window.rootViewController = GalleryViewController()
+        window.rootViewController = GalleryAssembly.build()
         self.window = window
         window.makeKeyAndVisible()
     }
--- Pixabay/Views/Gallery/GalleryAssembly.swift
+++ Pixabay/Views/Gallery/GalleryAssembly.swift
@@ -5,5 +5,20 @@
 //  Created by Ilia Tsikelashvili on 05.08.22.
 //
 
-import Foundation
+import UIKit
 
+struct GalleryAssembly {
+    
+    static func build() -> UIViewController {
+        
+        let networking = Networking()
+        let service = GalleryService(network: networking)
+        let viewModel = GalleryViewModel(service: service)
+        let view = GalleryContentView(viewModel: viewModel)
+        let controller = BaseViewController<GalleryViewModel, GalleryContentView>(rootView: view)
+        controller.viewModel = viewModel
+        controller.title = "Gallery"
+        return controller
+    }
+    
+}
