ac435702c01786bae65dbf4e4f3ff65af2792e2b
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 17:50:22 +0400

    Added protocol for BaseViewModel

--- Pixabay/Base/ViewModeling.swift
+++ Pixabay/Base/ViewModeling.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+protocol ViewModeling: AnyObject {
+    func initialSetup()
+    func viewWillAppear(_ animated: Bool)
+    func viewDidAppear(_ animated: Bool)
+    func viewWillDisappear(_ animated: Bool)
+}
