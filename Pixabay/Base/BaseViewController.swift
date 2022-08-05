fe6586a8e084a4f0e6c6225b2754982fb116a1ba
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 17:51:21 +0400

    Added BaseViewController
    Decision is to build ViewController with SwiftUI, thats why we will use UIHostingController

--- Pixabay/Base/BaseViewController.swift
+++ Pixabay/Base/BaseViewController.swift
@@ -5,4 +5,38 @@
 //  Created by Ilia Tsikelashvili on 05.08.22.
 //
 
-import Foundation
+import SwiftUI
+
+class BaseViewController<ViewModelType: ViewModeling, ViewType: View>: UIHostingController<ViewType> {
+
+    weak var viewModel: ViewModelType!
+    override var navigationController: UINavigationController? { nil }
+
+    override init(rootView: ViewType) {
+        super.init(rootView: rootView)
+        initialSetup()
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func initialSetup() {}
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.viewWillAppear(animated)
+
+        navigationController?.navigationBar.isTranslucent = true
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewModel.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.viewWillDisappear(animated)
+    }
+}
