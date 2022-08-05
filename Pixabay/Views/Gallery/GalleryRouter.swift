c5a617109b09cb4ceaa9e24e13efc0b8350635bb
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 21:41:02 +0400

    Added function for Gallery Router
    With modal presentation style we will go to the SelectedPhoto view

--- Pixabay/Views/Gallery/GalleryRouter.swift
+++ Pixabay/Views/Gallery/GalleryRouter.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+class GalleryRouter: BaseRouting {
+    
+    func openSelectedPhoto(with data: Hits) {
+        let module = SelectedPhotoAssembly.build(with: data).wrapped
+        topViewController?.show(module, sender: self)
+    }
+}
