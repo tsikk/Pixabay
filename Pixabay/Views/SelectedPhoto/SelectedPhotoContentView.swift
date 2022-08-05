cd6c641ea116d9ddb586a13d159449d5140e8d81
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 21:40:12 +0400

    Added SelctedPhotoContentView
    Temporary using Texts to see that passing the data is actually happening

---
+++ Pixabay/Views/SelectedPhoto/SelectedPhotoContentView.swift
@@ -0,0 +1,24 @@
+//
+//  SelctedPhotoContentView.swift
+//  Pixabay
+//
+//  Created by Ilia Tsikelashvili on 05.08.22.
+//
+
+import SwiftUI
+
+struct SelectedPhotoContentView: View {
+    @ObservedObject var viewModel: SelectedPhotoViewModel
+    
+    var body: some View {
+        Text("\(viewModel.data.id)")
+        Text("\(viewModel.data.downloads)")
+        Text("\(viewModel.data.pageURL)")
+    }
+}
+
+struct SelctedPhotoContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedPhotoContentView(viewModel: SelectedPhotoViewModel())
+    }
+}
