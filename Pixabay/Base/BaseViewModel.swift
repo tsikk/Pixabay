3580cb455b99ac126dcdd116c9d8e747d3f55cd1
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 17:50:35 +0400

    Added BaseViewModel

--- Pixabay/Base/BaseViewModel.swift
+++ Pixabay/Base/BaseViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class BaseViewModel: ViewModeling, ObservableObject {
+
+    required init() {
+        initialSetup()
+    }
+
+    func initialSetup() {}
+    func viewWillAppear(_ animated: Bool) {}
+    func viewDidAppear(_ animated: Bool) {}
+    func viewWillDisappear(_ animated: Bool) {}
+
+}
