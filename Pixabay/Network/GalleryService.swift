a9698ffdebb17a8330a2717ca93790386f0b47d0
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 18:56:23 +0400

    GalleryService for fetching the gallery and for the further use: selected photo

---
+++ Pixabay/Network/GalleryService.swift
@@ -0,0 +1,42 @@
+//
+//  GalleryService.swift
+//  Pixabay
+//
+//  Created by Ilia Tsikelashvili on 05.08.22.
+//
+
+import Foundation
+import Combine
+
+protocol GalleryServiceProtocol: AnyObject {
+    var network: NetworkingProtocol { get }
+
+    func getGallery() -> AnyPublisher<Gallery, Error>
+    func getPhoto(id: String) -> AnyPublisher<Gallery, Error>
+}
+
+final class GalleryService: GalleryServiceProtocol {
+    
+    let network: NetworkingProtocol
+    
+    init(network: NetworkingProtocol) {
+        self.network = network
+    }
+    
+    func getGallery() -> AnyPublisher<Gallery, Error> {
+        let endpoint = Endpoint.gallery
+        
+        return network.get(type: Gallery.self,
+                           url: endpoint.url,
+                           headers: endpoint.headers)
+    }
+
+    func getPhoto(id: String) -> AnyPublisher<Gallery, Error> {
+        let endpoint = Endpoint.photo(id: id)
+        
+        return network.get(type: Gallery.self,
+                           url: endpoint.url,
+                           headers: endpoint.headers)
+    }
+    
+}
