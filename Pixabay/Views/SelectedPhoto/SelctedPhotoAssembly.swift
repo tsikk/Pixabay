6a70a763734b253d219aaf1a148ea424622b66c9
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-05 21:40:23 +0400

    Added DI for SelectedPhoto

---
+++ Pixabay/Views/SelectedPhoto/SelctedPhotoAssembly.swift
@@ -0,0 +1,22 @@
+//
+//  SelctedPhotoAssembly.swift
+//  Pixabay
+//
+//  Created by Ilia Tsikelashvili on 05.08.22.
+//
+
+import UIKit
+
+struct SelectedPhotoAssembly {
+    
+    static func build(with data: Hits) -> UIViewController {
+        
+        let viewModel = SelectedPhotoViewModel(data: data)
+        let view = SelectedPhotoContentView(viewModel: viewModel)
+        let controller = BaseViewController<SelectedPhotoViewModel, SelectedPhotoContentView>(rootView: view)
+        controller.viewModel = viewModel
+        controller.title = "Photo"
+        return controller
+    }
+    
+}
