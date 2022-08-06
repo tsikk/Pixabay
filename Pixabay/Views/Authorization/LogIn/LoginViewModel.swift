928b6945c4fc264f5f07721912aa2554432a9634
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-06 14:44:49 +0400

    Updated LoginViewModel
    Here we signIn and also routing on the register view

--- Pixabay/Views/Authorization/LogIn/LoginViewModel.swift
+++ Pixabay/Views/Authorization/LogIn/LoginViewModel.swift
@@ -6,11 +6,30 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 class LoginViewModel: BaseViewModel {
+    
+    private let router = LoginRouter()
+    let auth = Auth.auth()
+     
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
+    func onSignIn(email: String, password: String)  {
+        auth.signIn(withEmail: email, password: password ) { result, error in
+            guard result != nil, error == nil else { return }
+            
+        }
+    }
+    
+    func onRegisterButton() {
+        router.openRegistration()
+    }
+  
 }
