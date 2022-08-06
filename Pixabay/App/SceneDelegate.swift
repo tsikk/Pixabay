423d6076876a56ffd2b0d16635ec86f993a59691
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-06 14:43:21 +0400

    Changed rootViewController from Gallery into Login

--- Pixabay/App/SceneDelegate.swift
+++ Pixabay/App/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         let window = UIWindow(windowScene: windowScene)
 
-        window.rootViewController = GalleryAssembly.build().wrapped
+        window.rootViewController = LoginAssembly.build().wrapped
         self.window = window
         window.makeKeyAndVisible()
     }
