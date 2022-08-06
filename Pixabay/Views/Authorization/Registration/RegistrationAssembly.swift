895b8e98686525d72996c4136d1f06ff79b924b9
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-06 14:45:04 +0400

    Updated Registration DI

--- Pixabay/Views/Authorization/Registration/RegistrationAssembly.swift
+++ Pixabay/Views/Authorization/Registration/RegistrationAssembly.swift
@@ -5,4 +5,16 @@
 //  Created by Ilia Tsikelashvili on 06.08.22.
 //
 
-import Foundation
+import UIKit
+
+struct RegistrationAssembly {
+    
+    static func build() -> UIViewController {
+        let viewModel = RegistrationViewModel()
+        let view = RegistrationContentView(viewModel: viewModel)
+        let controller = BaseViewController<RegistrationViewModel, RegistrationContentView>(rootView: view)
+        controller.viewModel = viewModel
+        return controller
+    }
+    
+}
